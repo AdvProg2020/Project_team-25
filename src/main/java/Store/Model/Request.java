f81d17c2e52682d81ca0ca4d91db898c901d7b60
Mahdi Salmani <m10.salmani@gmail.com>
2020-04-16 21:34:48 +0430

    add Request status and some Constructor changes

--- src/main/java/Store/Model/Request.java
+++ src/main/java/Store/Model/Request.java
@@ -1,32 +1,45 @@
 package Store.Model;
 
 import Store.Model.Enums.RequestType;
+import Store.Model.Enums.VerifyStatus;
 
 public class Request {
     private RequestType type; // 5 Types
+    private VerifyStatus status;
     private Product product;
     private Seller seller;
     private Offer offer;
+    private Object firstObject;
+    private Object secondObject;
     private boolean change;
 
     Request(Seller seller) {
         this.type = RequestType.REGISTER_SELLER;
         this.seller = seller;
+        this.status = VerifyStatus.WAITING;
     }
 
-    Request(Product product, boolean change) {
+    Request(Product product, boolean change, Object firstObject, Object secondObject) {
         this.product = product;
         this.change = change;
         this.type = RequestType.ADD_NEW_PRODUCT;
-        if (this.change)
+        if (this.change) {
             this.type = RequestType.CHANGE_PRODUCT;
+            this.firstObject = firstObject;
+            this.secondObject = secondObject;
+        }
+        this.status = VerifyStatus.WAITING;
     }
 
-    Request(Seller seller, Offer offer, boolean change) {
+    Request(Seller seller, Offer offer, boolean change, Object firstObject, Object secondObject) {
         this.offer = offer;
         this.change = change;
         this.type = RequestType.ADD_NEW_OFFER;
-        if (this.change)
+        if (this.change) {
             this.type = RequestType.CHANGE_OFFER;
+            this.firstObject = firstObject;
+            this.secondObject = secondObject;
+        }
+        this.status = VerifyStatus.WAITING;
     }
 }
