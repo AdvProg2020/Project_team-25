9acb934bb11139056e22883a60c304fded70122d
Mahdi Salmani <m10.salmani@gmail.com>
2020-04-30 23:44:26 +0430

    change format of SignUpAndLoginController&Menu

--- src/main/java/Store/View/SignUpAndLoginMenu.java
+++ src/main/java/Store/View/SignUpAndLoginMenu.java
@@ -6,6 +6,7 @@ import Store.Model.Manager;
 import Store.Model.User;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.regex.Matcher;
 
 public class SignUpAndLoginMenu {
@@ -16,61 +17,40 @@ public class SignUpAndLoginMenu {
     public static void init() {
         String input;
         Matcher matcher;
-        System.out.println("\nSign up and login menu\n");
-        while(!(input = InputManager.getNextLine()).equalsIgnoreCase("back")) {
-            if((matcher = InputManager.getMatcher(input, CREATE_ACCOUNT_REGEX)).find()) {
+        System.out.println("\nSign up and Login menu\n");
+        while (!(input = InputManager.getNextLine()).equalsIgnoreCase("back")) {
+            if ((matcher = InputManager.getMatcher(input, CREATE_ACCOUNT_REGEX)).find()) {
                 handleCreateAccount(matcher.group(1), matcher.group(2));
-            }
-            else if((matcher = InputManager.getMatcher(input, LOGIN_REGEX)).find()) {
+            } else if ((matcher = InputManager.getMatcher(input, LOGIN_REGEX)).find()) {
                 if (handleLogin(matcher.group(1))) {
-                    System.out.println("Login successful");
                     break;
                 }
-            }
-            else if(input.equalsIgnoreCase("help")) {
+            } else if (input.equalsIgnoreCase("help")) {
                 printHelp();
-            }
-            else if(input.equalsIgnoreCase("offs")) {
+            } else if (input.equalsIgnoreCase("offs")) {
                 OffersMenu.init();
-            }
-            else if(input.equalsIgnoreCase("products")) {
+            } else if (input.equalsIgnoreCase("products")) {
                 ProductsMenu.init();
-            }
-            else {
-                System.out.println("invalid command");
+            } else {
+                System.out.println("Invalid command!");
             }
         }
     }
 
     private static void handleCreateAccount(String type, String username) {
-        if (User.getUserByUsername(username) != null) {
-            System.out.println("A user with this username already exists");
-            return;
-        }
-        if (type.equalsIgnoreCase("manager") && Manager.hasManager) {
-            System.out.println("You can only create the first manager via this method");
-            return;
-        }
-        ArrayList<String> attributes = getPersonal(username);
-        if (type.equalsIgnoreCase("seller")) {
-            double money = getMoney();
-            String companyName, companyDescription;
-            System.out.println("Company Name: ");
-            companyName = InputManager.getNextLine();
-            System.out.println("Company Description: ");
-            companyDescription = InputManager.getNextLine();
-
-            SignUpAndLoginController.createSeller(attributes, money, companyName, companyDescription);
-        }
-        else if (type.equalsIgnoreCase("customer")) {
-            SignUpAndLoginController.createCustomer(attributes, getMoney());
-        }
-        else {
-            SignUpAndLoginController.createManager(attributes);
-        }
+        System.out.println(SignUpAndLoginController.handleCreateAccount(type, username));
     }
 
-    private static double getMoney() {
+    public static ArrayList<String> getCompanyDescription() {
+        String companyName, companyDescription;
+        System.out.println("Company Name: ");
+        companyName = InputManager.getNextLine();
+        System.out.println("Company Description: ");
+        companyDescription = InputManager.getNextLine();
+        return new ArrayList(Arrays.asList(companyName, companyDescription));
+    }
+
+    public static double getMoney() {
         System.out.println("Initial Money: ");
         String input;
         while (!(input = InputManager.getNextLine()).matches("^\\d+(.\\d+)?$")) {
@@ -79,7 +59,7 @@ public class SignUpAndLoginMenu {
         return Double.parseDouble(input);
     }
 
-    private static ArrayList<String> getPersonal(String username) {
+    public static ArrayList<String> getPersonal(String username) {
         String firstName, lastName, email, phoneNumber, password;
         System.out.println("Password: ");
         password = InputManager.getNextLine();
@@ -89,44 +69,33 @@ public class SignUpAndLoginMenu {
         lastName = InputManager.getNextLine();
         System.out.println("Email: ");
         while (!(email = InputManager.getNextLine()).matches("[^\\s]+@[^\\s]+(\\.)com")) {
-            System.out.println("Invalid format");
+            System.out.println("Invalid format!");
         }
         System.out.println("Phone Number: ");
         while (!(phoneNumber = InputManager.getNextLine()).matches("^[0-9]+$")) {
-            System.out.println("Invalid format");
+            System.out.println("Invalid format!");
         }
-        ArrayList<String> res = new ArrayList<String>();
-        res.add(username);
-        res.add(firstName);
-        res.add(lastName);
-        res.add(email);
-        res.add(phoneNumber);
-        res.add(password);
+        ArrayList<String> res = new ArrayList<String>(Arrays.asList(username, firstName, lastName, email, phoneNumber, password));
         return res;
     }
 
     private static boolean handleLogin(String username) {
-        User user = User.getUserByUsername(username);
-        if (user == null) {
-            System.out.println("No user with this username exists");
-            return false;
-        }
-
         System.out.println("Please enter your password: ");
         String password = InputManager.getNextLine();
-        if (user.validatePassword(password)) {
-            MainMenu.currentUser = user;
+        String message = SignUpAndLoginController.handleLogin(username, password);
+        System.out.println(message);
+        if (message.equals("Login successful.")) {
             return true;
         }
-        else {
-            System.out.println("Invalid password");
-            return false;
-        }
+        return false;
     }
 
     private static void printHelp() {
         System.out.println("create account [type] [username]");
         System.out.println("login [username]");
+        System.out.println("help");
+        System.out.println("offs");
+        System.out.println("products");
         System.out.println("back");
     }
 }
